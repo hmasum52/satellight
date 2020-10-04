3204dfc2c109094d7563266b4afb8fda30c43280
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-10-04 17:56:03 +0600

    Added Direction: NW NE ..

--- app/src/main/java/github/hmasum18/satellight/views/VrViewFragment.java
+++ app/src/main/java/github/hmasum18/satellight/views/VrViewFragment.java
@@ -48,6 +48,7 @@ public class VrViewFragment extends Fragment {
     private VrPanoramaView mVRPanoramaView;
     private ImageView satelliteView;
     private TextView directionText;
+    private TextView hLocationText;
 
     private int index = 2;
 
@@ -82,6 +83,7 @@ public class VrViewFragment extends Fragment {
         satelliteView = rootView.findViewById(R.id.satellight);
         mArduinoConnectBtn = rootView.findViewById(R.id.vrViewFrag_arduinoConnectBtn);
         directionText = rootView.findViewById(R.id.direction_text);
+        hLocationText = rootView.findViewById(R.id.h_location_postion);
 
         satelliteView.setImageResource(R.drawable.satellite_mono);
 
@@ -204,13 +206,22 @@ public class VrViewFragment extends Fragment {
         rotation.setListener(cl);
     }
 
+
+    static final float DEVIATION = 10;
     private Rotation.RotationListener getRotationListener() {
         return (azimuth, vAngle) -> runOnUiThread(() -> {
             float m_azimuth = azimuth + 90;
-            final float DEVIATION = 10;
 
-            if ((m_azimuth>=360-DEVIATION && m_azimuth<=360) || (m_azimuth>=0 && m_azimuth<=DEVIATION))
+            if ((m_azimuth>=360-DEVIATION && m_azimuth<=360+DEVIATION) || (m_azimuth>=-DEVIATION && m_azimuth<=DEVIATION))
                 directionText.setText("N");
+            else if (m_azimuth>=315-DEVIATION && m_azimuth<=315+DEVIATION)
+                directionText.setText("NW");
+            else if (m_azimuth>=225-DEVIATION && m_azimuth<=225+DEVIATION)
+                directionText.setText("SW");
+            else if (m_azimuth>=135-DEVIATION && m_azimuth<=135+DEVIATION)
+                directionText.setText("SE");
+            else if (m_azimuth>=405-DEVIATION && m_azimuth<=405+DEVIATION)
+                directionText.setText("NE");
             else if (m_azimuth>=90-DEVIATION && m_azimuth<=90+DEVIATION)
                 directionText.setText("E");
             else if (m_azimuth>=180-DEVIATION && m_azimuth<=180+DEVIATION)
@@ -219,6 +230,10 @@ public class VrViewFragment extends Fragment {
                 directionText.setText("W");
             else
                 directionText.setText("");
+
+            ConstraintLayout.LayoutParams par = (ConstraintLayout.LayoutParams) hLocationText.getLayoutParams();
+            par.horizontalBias = (vAngle+90.0f)/180;
+            hLocationText.setLayoutParams(par);
 
             Pair<Float, Float> pair = satellite.getPosition(azimuth, vAngle);
             if (pair != null) {
